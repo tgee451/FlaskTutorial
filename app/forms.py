796cd86bc65f555714a6e20c2a7cc5d6092f8028
tgee451 <tgee451@gmail.com>
2021-02-22 18:48:40 -0500

    Completed Chapter 10 of Tutorial (Email)

--- app/forms.py
+++ app/forms.py
@@ -50,3 +50,13 @@ class PostForm(FlaskForm):
         DataRequired(), Length(min=1, max=140)
     ])
     submit = SubmitField('Submit')
+
+class ResetPassswordRequestForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    submit = SubmitField('Request Password Reset')
+
+class ResetPasswordForm(FlaskForm):
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField(
+        'Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Request Password Reset')
